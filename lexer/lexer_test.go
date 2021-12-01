9e7eb3dfffd91992edf0c22c6f584cdaab50a21b
Sam Folorunsho <sam@sams-imac.mynet>
2021-12-01 12:22:56 +0000

    finish setup

---
+++ lexer/lexer_test.go
@@ -0,0 +1,40 @@
+package lexer_test
+
+import (
+	"testing"
+
+	"monkey/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){},;`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tokenTest := range tests {
+		currentToken := l.NextToken()
+
+		if currentToken.Type != tokenTest.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tokenTest.expectedType, currentToken.Type)
+		}
+
+		if currentToken.Literal != tokenTest.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tokenTest.expectedLiteral, currentToken.Literal)
+		}
+	}
+}
---
+++ main.go
@@ -0,0 +1,14 @@
+package monkey
+
+import (
+	"monkey/lexer"
+	"monkey/token"
+)
+
+func main() {
+	// token
+	token.AND_ASSIGN
+
+	// lexer
+	lexer.New
+}
