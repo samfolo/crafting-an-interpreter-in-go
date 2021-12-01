a8ba30023db8b45a84f04fd89821408a15739787
Sam Folorunsho <sam@sams-imac.mynet>
2021-12-01 13:16:43 +0000

    add the ability to skip whitespace

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -24,12 +24,15 @@ func (lex *Lexer) readChar() {
 	} else {
 		lex.ch = lex.input[lex.readPosition]
 	}
+
 	lex.position = lex.readPosition
 	lex.readPosition += 1
 }
 
 func (lex *Lexer) NextToken() token.Token {
 	var tok token.Token
+
+	lex.skipWhitespace()
 
 	switch lex.ch {
 	case '=':
@@ -51,6 +54,14 @@ func (lex *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(lex.ch) {
+			tok.Literal = lex.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, lex.ch)
+		}
 	}
 
 	lex.readChar()
@@ -59,4 +70,25 @@ func (lex *Lexer) NextToken() token.Token {
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (lex *Lexer) readIdentifier() string {
+	position := lex.position
+
+	for isLetter(lex.ch) {
+		lex.readChar()
+	}
+
+	return lex.input[position:lex.position]
+}
+
+// what can be used in variable and function names
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func (lex *Lexer) skipWhitespace() {
+	for lex.ch == ' ' || lex.ch == '\t' || lex.ch == '\n' || lex.ch == '\r' {
+		lex.readChar()
+	}
 }
--- token/token.go
+++ token/token.go
@@ -28,3 +28,17 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+// check if a variable is a reserved word or not:
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+
+	return IDENT
+}
