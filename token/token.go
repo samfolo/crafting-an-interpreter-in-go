3b5a4ffa2eb8ebf6cb573c15c39f9eb149da883c
Sam Folorunsho <sam@sams-imac.mynet>
2021-12-01 13:44:06 +0000

    lexer: add if, else, true, false, return

--- token/token.go
+++ token/token.go
@@ -8,12 +8,15 @@ type Token struct {
 }
 
 const (
+	// special
 	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"
 
+	// primitive entities
 	IDENT = "IDENT" // function names, variable names, etc.
 	INT   = "INT"
 
+	// operators
 	ASSIGN   = "="
 	PLUS     = "+"
 	MINUS    = "-"
@@ -21,24 +24,38 @@ const (
 	ASTERISK = "*"
 	SLASH    = "/"
 
+	// comparators
 	LT = "<"
 	GT = ">"
 
+	// delimiters
 	COMMA     = ","
 	SEMICOLON = ";"
 
+	// scoping
 	LPAREN = "("
 	RPAREN = ")"
 	LBRACE = "{"
 	RBRACE = "}"
 
+	// reserved words
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 // check if a variable is a reserved word or not:
