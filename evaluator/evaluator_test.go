6da035c7ef0db2657d7c920ba849f047e0550ab2
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2021-12-04 15:48:05 +0000

    evaluator: evaluate the `!` operator

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -75,3 +75,22 @@ func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
 
 	return true
 }
+
+func TestBangOAperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+	}
+
+	for _, test := range tests {
+		evaluated := testEval(test.input)
+		testBooleanObject(t, evaluated, test.expected)
+	}
+}
