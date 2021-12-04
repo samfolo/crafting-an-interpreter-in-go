17e6d1c005401b528985a8d970c9bce7a7e9f191
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2021-12-04 13:49:11 +0000

    object: add `Integer` object type

---
+++ object/object.go
@@ -0,0 +1,23 @@
+package object
+
+import (
+	"fmt"
+)
+
+type ObjectType string
+
+const (
+	INTEGER_OBJ = "INTEGER"
+)
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
+func (i *Integer) Type() string    { return INTEGER_OBJ }
