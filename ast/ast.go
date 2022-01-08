fb57a4697eaeb070c2e94f79a40b0ddf901057ed
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2022-01-08 21:52:17 +0000

    ast: add HashLiteral ast node

--- ast/ast.go
+++ ast/ast.go
@@ -295,7 +295,7 @@ func (al *ArrayLiteral) String() string {
 }
 
 type IndexExpression struct {
-	Token token.Token // will be token.LBRACE
+	Token token.Token // will be token.LBRACKET
 	Left  Expression
 	Index Expression
 }
@@ -310,6 +310,29 @@ func (ie *IndexExpression) String() string {
 	out.WriteString("[")
 	out.WriteString(ie.Index.String())
 	out.WriteString("])")
+
+	return out.String()
+}
+
+type HashLiteral struct {
+	Token token.Token // will be token.LBRACE
+	Pairs map[Expression]Expression
+}
+
+func (hl *HashLiteral) expressionNode()      {}
+func (hl *HashLiteral) TokenLiteral() string { return hl.Token.Literal }
+func (hl *HashLiteral) String() string {
+	var out bytes.Buffer
+
+	pairs := []string{}
+
+	for key, value := range hl.Pairs {
+		pairs = append(pairs, key.String()+":"+value.String())
+	}
+
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
 
 	return out.String()
 }
