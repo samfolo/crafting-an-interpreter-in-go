c9bccfeb0a817b0cb403f680b50c9347981e3031
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2022-01-08 19:38:00 +0000

    ast: support index expressions

--- ast/ast.go
+++ ast/ast.go
@@ -293,3 +293,22 @@ func (al *ArrayLiteral) String() string {
 
 	return out.String()
 }
+
+type IndexExpression struct {
+	Token token.Token // will be token.LBRACE
+	Left  Expression
+	Index Expression
+}
+
+func (ie *IndexExpression) expressionNode()      {}
+func (ie *IndexExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *IndexExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ie.Left.String())
+	out.WriteString("[")
+	out.WriteString(ie.Index.String())
+	out.WriteString("]")
+
+	return out.String()
+}
